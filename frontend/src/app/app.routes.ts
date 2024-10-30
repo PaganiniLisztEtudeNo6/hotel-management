9e206ff002455907f86ba6cfb9caf411a5c31f9c
Donovan <teeseeker1015@gmail.com>
2024-10-30 16:14:00 +0700

    create route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { Page1Component } from './page/page1/page1.component';
+import { Page2Component } from './page/page2/page2.component';
+import { Page3Component } from './page/page3/page3.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component },
+  { path: 'page3', component: Page3Component },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
