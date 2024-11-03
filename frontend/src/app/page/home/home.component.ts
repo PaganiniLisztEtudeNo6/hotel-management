5197cd167a229755591e9af8e2261f0a25b79343
PloyPornpailin <s6402041610030@email.kmutnb.ac.th>
2024-11-04 01:34:45 +0700

    img

--- frontend/src/app/page/home/home.component.ts
+++ frontend/src/app/page/home/home.component.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
-
-
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports:
-    [ButtonModule,
-      CardModule
-    ],
+  imports: [
+    ButtonModule,
+    CardModule,
+    CarouselModule
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  images: string[] = [
+    'https://img.freepik.com/free-photo/luxury-classic-modern-bedroom-suite-hotel_105762-1787.jpg?t=st=1730657787~exp=1730661387~hmac=5bbb626823ed7218103b25e84a4e0cc9a5e0c2fae3e87dec827cc77795244065&w=996'
+    
+  ];
 }
