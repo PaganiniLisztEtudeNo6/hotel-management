44bfdd6ad278ad4ca7cb7a7fda4eeb64375a0a20
PloyPornpailin <s6402041610030@email.kmutnb.ac.th>
2024-11-05 23:18:19 +0700

    navbar2

--- frontend/src/app/page/home/home.component.ts
+++ frontend/src/app/page/home/home.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { CarouselModule } from 'primeng/carousel';
+import { MenubarModule } from 'primeng/menubar';  // เพิ่ม MenubarModule เพื่อใช้งาน p-menubar
+
+
 
 @Component({
   selector: 'app-home',
@@ -9,7 +12,8 @@ import { CarouselModule } from 'primeng/carousel';
   imports: [
     ButtonModule,
     CardModule,
-    CarouselModule
+    CarouselModule,
+    MenubarModule  // เพิ่ม MenubarModule ใน imports
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
@@ -17,6 +21,5 @@ import { CarouselModule } from 'primeng/carousel';
 export class HomeComponent {
   images: string[] = [
     'https://img.freepik.com/free-photo/luxury-classic-modern-bedroom-suite-hotel_105762-1787.jpg?t=st=1730657787~exp=1730661387~hmac=5bbb626823ed7218103b25e84a4e0cc9a5e0c2fae3e87dec827cc77795244065&w=996'
-    
   ];
 }
