de2bbb3141eae60876761acb7a34dd0d0d415495
PloyPornpailin <s6402041610030@email.kmutnb.ac.th>
2024-11-14 21:25:12 +0700

    page3

--- frontend/src/app/page/page2/page2.component.ts
+++ frontend/src/app/page/page2/page2.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { MenubarModule } from 'primeng/menubar';  // เพิ่ม MenubarModule เพื่อใช้งาน p-menubar
 
 @Component({
   selector: 'app-page2',
@@ -8,7 +9,8 @@ import { CardModule } from 'primeng/card';
 
   imports: [
     ButtonModule,
-    CardModule
+    CardModule,
+    MenubarModule  // เพิ่ม MenubarModule ใน imports
   ],
   templateUrl: './page2.component.html',
   styleUrl: './page2.component.css'
--- frontend/src/app/page/page3/page3.component.ts
+++ frontend/src/app/page/page3/page3.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { CarouselModule } from 'primeng/carousel';
+import { MenubarModule } from 'primeng/menubar';  // เพิ่ม MenubarModule เพื่อใช้งาน p-menubar
 
 @Component({
   selector: 'app-page3',
   standalone: true,
   imports: [
     ButtonModule,
+    CardModule,
+    MenubarModule  // เพิ่ม MenubarModule ใน imports
   ],
   templateUrl: './page3.component.html',
-  styleUrl: './page3.component.css'
+  styleUrls: ['./page3.component.css']
 })
 export class Page3Component {
-
+  // กำหนดตัวแปร imageUrl เพื่อเก็บ URL ของรูปภาพ
+  imageUrl: string = 'https://img.freepik.com/free-photo/luxury-classic-modern-bedroom-suite-hotel_105762-1787.jpg?t=st=1730657787~exp=1730661387~hmac=5bbb626823ed7218103b25e84a4e0cc9a5e0c2fae3e87dec827cc77795244065&w=996';
 }
