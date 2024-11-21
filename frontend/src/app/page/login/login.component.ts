beff4eda01e91da2798f981e65f50059971c49ca
Donovan <teeseeker1015@gmail.com>
2024-11-21 22:28:14 +0700

    fix

--- frontend/src/app/page/login/login.component.ts
+++ frontend/src/app/page/login/login.component.ts
@@ -1,23 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, InputTextModule, ButtonModule],
+  imports: [],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  username: string = '';
-  password: string = '';
 
-  onSubmit() {
-    console.log('Username:', this.username);
-    console.log('Password:', this.password);
-    // Logic สำหรับการตรวจสอบข้อมูลการ login สามารถใส่ที่นี่
-  }
 }
